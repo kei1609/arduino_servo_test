b9b6c2a0b34ee984c1931fe9fd634c3cae1010e1
itak <itak0916@gmail.com>
2023-12-03 20:53:24 +0900

    サーボモータをSG90 → GeekServo 9g 360°に変更

--- arduino_servo_test/arduino_servo_test.ino
+++ arduino_servo_test/arduino_servo_test.ino
@@ -5,19 +5,25 @@ const int SERVO_PIN = 9;
 
 Servo servo;
 int pos = 0;
+int potval;
+int servo_pwm;
 
 void setup() {
   servo.attach(SERVO_PIN);
-  //pinMode(BUTTON_PIN, INPUT); 
+  //pinMode(BUTTON_PIN, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos++) {
-    servo.write(pos);
-    delay(15);
-  }
-  for (pos = 180; pos <= 0; pos--) {
-    servo.write(pos);
-    delay(15);
-  }
+  potval = analogRead(A0);
+  servo_pwm = map(potval, 0, 1023, 600, 2400);
+
+  servo.writeMicroseconds(servo_pwm);
+  delay(100);
+
+  Serial.print(potval);
+  Serial.print(": ");
+  Serial.print(servo_pwm);
+  Serial.println("[us]");
+
 }
